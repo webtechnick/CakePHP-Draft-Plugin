a0dc0b65e87d857892cecedb7edecd4932959428
Nick Baker <nick@webtechnick.com>
2014-02-19 16:31:17 -0700

    Adding phpdoc

--- View/Helper/DraftHelper.php
+++ View/Helper/DraftHelper.php
@@ -1,8 +1,28 @@
 <?php
 /**
 * Draft Helper
-*
 * requires jQuery
+*
+Load the helper
+@example
+public $helpers = array('Draft.Draft'); //Default settings. 
+public $helpers = array('Draft.Draft' => array(
+	'buffer' => true,
+	'draftPath' => array('admin' => false, 'plugin' => 'draft', 'controller' => 'drafts', 'action' => 'save'),
+	'discardPath' => array('admin' => false, 'plugin' => 'draft', 'controller' => 'drafts', 'action' => 'delete') 
+));
+
+Draft Link
+@example
+echo $this->Draft->button('Save Draft', array(
+	'model' => 'ModelName',
+	'form_id' => 'ModelForm'
+));
+echo $this->Draft->button('Save Draft', array(
+	'model' => 'ModelName',
+	'form_id' => 'ModelForm',
+	'script' => false // don't autoload javascript to execute saves.
+));
 *
 * @author Nick Baker
 * @since 1.0
@@ -20,6 +40,16 @@ class DraftHelper extends AppHelper {
 	public $discardPath = array('admin' => false, 'plugin' => 'draft', 'controller' => 'drafts', 'action' => 'delete');
 	public $buffer = true;
 	
+	/**
+	* Save Draft Button
+	* @param string text for link
+	* @param array of options
+	* - id: String id of button, required (default DraftButton)
+	* - model: String of model name to save against (default null)
+	* - script: Boolean build the jquery javascript (default true)
+	* - form_id: String of form data to serialize and submit for drafts (default null)
+	* @return string button link.
+	*/
 	public function button($text = 'Save Draft', $options = array()) {
 		$options = array_merge(array(
 			'id' => 'DraftButton',
@@ -38,11 +68,11 @@ class DraftHelper extends AppHelper {
 		$retval = $this->Html->link($text, $path, $options);
 		if ($script) {
 			$url = Router::url($path);
-			$js = '$("#'. $options['id'] .'").click(function(){
-				$.ajax({
+			$js = 'jQuery.("#'. $options['id'] .'").click(function(){
+				jQuery.ajax({
 					type:"POST",
 					url: "'. $url .'",
-					data: $("#'. $form_id .'").serialize(),
+					data: jQuery.("#'. $form_id .'").serialize(),
 					success: function(){
 						alert("Draft Saved.");
 					}
@@ -58,11 +88,20 @@ class DraftHelper extends AppHelper {
 		return $retval;
 	}
 	
+	/**
+	* Discard the draft button
+	* @param string text (default Discard Draft)
+	* @param array of options
+	* - id: String id of button, required (default DiscardButton)
+	* - model: String of model name to save against (default null)
+	* - model_id: String of model_id to delete against, (default null)
+	* @return string link to discard
+	*/
 	public function discard($text = 'Discard Draft', $options = array()) {
 		$options = array_merge(array(
 			'id' => 'DiscardButton',
 			'model' => '',
-			'model_id' => ''
+			'model_id' => null
 		), (array) $options);
 		
 		$model_id = $options['model_id'];
@@ -75,6 +114,14 @@ class DraftHelper extends AppHelper {
 		return $this->Html->link($text, $path, $options);
 	}
 	
+	/**
+	* AutoDraft intervals
+	* @param int seconds (default 60)
+	* @param array of options
+	* - model: String of model name to save against (default null)
+	* - form_id: String of form data to serialize and submit for drafts (default null)
+	* @return scriptblock or buffer interval to auto draft
+	*/
 	public function autoDraft($interval = 60 /* seconds */, $options = array()) {
 		$options = array_merge(array(
 			'model' => '',
@@ -85,10 +132,10 @@ class DraftHelper extends AppHelper {
 		$path[] = $options['model'];
 		$url = Router::url($path);
 		$js = 'setInterval(function(){
-			$.ajax({
+			jQuery.ajax({
 				type:"POST",
 				url: "'. $url .'",
-				data: $("#'. $options['form_id'] .'").serialize()
+				data: jQuery.("#'. $options['form_id'] .'").serialize()
 			});
 			return false;
 		}, '. $interval * 1000 .');';
