e0d6d4e8c4e68d266adf71752c5f53eb0a4aa747
Nick Baker <nick@webtechnick.com>
2014-02-18 22:09:58 -0700

    Adding schema data

---
+++ Config/Schema/schema.php
@@ -0,0 +1,30 @@
+<?php 
+class DraftSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $drafts = array(
+		'id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 36, 'key' => 'primary', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'model_id' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 36, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'model' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'user_id' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 26, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'key' => 'index'),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'key' => 'index'),
+		'json' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'model_id' => array('column' => 'model_id', 'unique' => 0),
+			'model' => array('column' => 'model', 'unique' => 0),
+			'user_id' => array('column' => 'user_id', 'unique' => 0),
+			'created' => array('column' => 'created', 'unique' => 0),
+			'modified' => array('column' => 'modified', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+}
